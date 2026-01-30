2ecc50a1382b0c06c371bb0317dfd71441591c8c
snehithareddy1444 <25495a4406@qiscet.edu.in>
2026-01-30 06:06:29 +0530

    Create HomeActivity.kt

---
+++ app/src/main/java/com/example/casestudy1/HomeActivity.kt
@@ -0,0 +1,10 @@
+package com.example.casestudy1
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
